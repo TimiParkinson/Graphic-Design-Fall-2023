8a53eecd7b30e9c7a031b72fb2ae12706ac68b1b
TimiParkinson <119539277+TimiParkinson@users.noreply.github.com>
2023-12-12 18:52:12 +0000

    Completed position page

---
+++ src/lib/Positioning.svelte
@@ -0,0 +1,41 @@
+<script>
+
+</script>
+
+<div>
+    <p>I stand out because I am a black dude who doesn't "act black". Often times my race card is confiscated and people question my sexuality because—as I've been told—something about screams "gay white woman". Whatever the hell that means... I'm here to say I'm a straight black man. Ok? Cool. </p>
+    <div>
+        <img src="/IMG_0666.jpg" alt="Me in front of a painting.">
+        <img src="/IMG_1957.jpg" alt="Me behind books.">
+    </div>
+</div>
+
+
+<style lang="scss">
+    div {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        div {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            height: 100%;
+        }
+    }
+    p {
+        font-size: 1.5vw;
+        text-align: center;
+    }
+    img {
+        position: relative;
+        display: block;
+        width: 25%;
+        height: 25%;
+    }
+</style>
